b5dfe1f6f213d1e6c4bf086c8b7011f85ee570ca
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-06-12 09:44:35 +0700

    feat(router-api): Support /v1/:chainId/dexs

--- apps/router-api/src/chains.ts
+++ apps/router-api/src/chains.ts
@@ -1,7 +1,30 @@
-// Enum to keeptrack supported chains
 enum Chain {
     Mainnet = 1,
     Arbitrum = 42161,
 }
 
-export { Chain };
+interface DEX {
+    name: string;
+}
+
+const uniswapV2: DEX = {
+    name: "Uniswap V2",
+};
+
+const sushiswap: DEX = {
+    name: "Sushiswap",
+};
+
+const curveV1: DEX = {
+    name: "Curve V1",
+};
+
+const dexs = {};
+dexs[Chain.Mainnet] = [uniswapV2, sushiswap, curveV1];
+dexs[Chain.Arbitrum] = [sushiswap, curveV1];
+
+function getSupportedDexs(chainId: string): Array<DEX> {
+    return dexs[chainId];
+}
+
+export { Chain, getSupportedDexs };
