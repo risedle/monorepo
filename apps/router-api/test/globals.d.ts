1e825d0356d9cca9f237c6535896c6e88d45b986
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-06-12 07:20:05 +0700

    fix(apps): Formatting

--- apps/router-api/test/globals.d.ts
+++ apps/router-api/test/globals.d.ts
@@ -1,6 +1,8 @@
 declare global {
     function getMiniflareBindings(): Bindings;
-    function getMiniflareDurableObjectStorage(id: DurableObjectId): Promise<DurableObjectStorage>;
+    function getMiniflareDurableObjectStorage(
+        id: DurableObjectId
+    ): Promise<DurableObjectStorage>;
 }
 
 export {};
