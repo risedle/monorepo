98c4fd956b8bc9c20ce2c3fb2849772b380775f3
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-06-16 18:19:47 +0700

    feat(api): implement /v1/quotes query params validation

--- apps/api/src/controllers/quotes.ts
+++ apps/api/src/controllers/quotes.ts
@@ -1,4 +1,41 @@
 import { Request, Response } from "express";
+import { query } from "express-validator/check";
+import { ethers, BigNumber } from "ethers";
+
+/**
+ * GetQuotes request query params validation
+ */
+const GetQuotesValidation = [
+    query("chainId")
+        .exists({ checkFalsy: true })
+        .isNumeric()
+        .withMessage("Invalid chainId")
+        .custom((value) => value in Chain)
+        .withMessages("chainId not supported"),
+    query("tokenIn")
+        .exists({ checkFalsy: true })
+        .withMessage("Invalid tokenIn")
+        .isEthereumAddress()
+        .trim()
+        .withMessages("Invalid tokenIn address")
+        .customSanitizer((value) => ethers.utils.getAddress(value))
+        .custom((value) => value in getTokens(req.query.chainId))
+        .withMessage("tokenIn not supported in chainId"),
+    query("tokenOut")
+        .exists({ checkFalsy: true })
+        .withMessage("Invalid tokenOut")
+        .isEthereumAddress()
+        .trim()
+        .withMessages("Invalid tokenIn address")
+        .customSanitizer((value) => ethers.utils.getAddress(value))
+        .custom((value) => value in getTokens(req.query.chainId))
+        .withMessage("tokenIn not supported in chainId"),
+    query("amountIn")
+        .exists({ checkFalsy: true })
+        .withMessage("Invalid amountIn")
+        .customSanitizer((value) => BigNumber.from(value))
+        .withMessage("amountIn is not BigNumberish"),
+];
 
 /**
  * GetQuotes return the best quotes for exchanging tokenIn for tokenOut
