7f6d0d1edf489830560223079fa4bcb5bd6de5a8
pyk <bayualdiyansyah@gmail.com>
2022-06-18 22:43:35 +0700

    feat(@risedle/chains): support binance smart chain (#37)
    * feat(@risedle/chains): support binance smart chain
    
    * fix(tsconfig): use node16 to support exports field in package.json
    
    * feat(@risedle/chains): update @risedle/types
    
    * chore(@risedle/chains): update release rules
    
    * chore(@risedle/chains): update test for bsc

--- packages/chains/src/index.ts
+++ packages/chains/src/index.ts
@@ -1,1 +1,13 @@
-export * from "./chains";
+import { Binance } from "./binance";
+import { ChainId } from "@risedle/types/chain";
+
+export const ALL_CHAINS = [Binance];
+export const ALL_CHAIN_IDS = ALL_CHAINS.map((chain) => chain.id);
+
+// Check wether chain id is supported or not
+export function isChainIdSupported(chainId: ChainId): boolean {
+    return ALL_CHAIN_IDS.includes(chainId);
+}
+
+// Export all chains
+export * from "./binance";
