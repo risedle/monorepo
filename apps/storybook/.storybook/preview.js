c22b6b71f8cd3052d8e3e78d557e3e559ad28b43
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-06-24 00:52:15 +0700

    chore(storybook): setup tailwind and @risedle/ui

--- apps/storybook/.storybook/preview.js
+++ apps/storybook/.storybook/preview.js
@@ -1,3 +1,5 @@
+import "../stories/tailwind.css";
+
 export const parameters = {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
@@ -6,4 +8,5 @@ export const parameters = {
             date: /Date$/,
         },
     },
+    layout: "fullscreen",
 };
