f6916529708c08ccfa95e95225efb68f1e01bbe2
pyk <bayualdiyansyah@gmail.com>
2022-06-24 04:23:25 +0700

    chore(storybook): remove unused stories (#57)
    * docs(storybook): refer @risedle/ui to create new component
    
    * chore(storybook): support mdx only
    
    * chore(storybook): remove Button
    
    * chore(storybook): remove Header
    
    * chore(storybook): remove Page

--- apps/storybook/.storybook/main.js
+++ apps/storybook/.storybook/main.js
@@ -1,8 +1,5 @@
 module.exports = {
-    stories: [
-        "../stories/**/*.stories.mdx",
-        "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-    ],
+    stories: ["../stories/**/*.stories.mdx"],
     addons: [
         "@storybook/addon-links",
         "@storybook/addon-essentials",
