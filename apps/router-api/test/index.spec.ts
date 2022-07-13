c496337274642746279e3a48ea3c54f487ea6d92
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-07-13 22:12:48 +0700

    chore(apps/router-api): remove router-api apps

--- apps/router-api/test/index.spec.ts
+++
@@ -1,8 +0,0 @@
-import { worker } from "@/index";
-
-it("should responds 200 OK", async () => {
-    const env = getMiniflareBindings();
-    const res = await worker.fetch(new Request("http://localhost"), env);
-    expect(res.status).toBe(200);
-    expect(await res.json()).toEqual({ message: "OK" });
-});
