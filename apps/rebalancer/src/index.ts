0bd225097c28d9bb24bc44f762fd2218d88a67e5
pyk <bayualdiyansyah@gmail.com>
2022-07-13 13:10:03 +0700

    feat(apps/rebalancer): implement getRebalances (#81)
    * feat(apps/rebalancer): implement getRebalances
    
    * chore(apps/rebalancer): reorder .env.example

--- apps/rebalancer/src/index.ts
+++ apps/rebalancer/src/index.ts
@@ -1,3 +1,14 @@
-console.log("rebalancer");
+import { ethers } from "ethers";
+import { RPC_URL, FLTS, MIN_PROFITS } from "./config";
+import { getRebalances } from "./rebalance";
 
-export {};
+// Initialize provider
+const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
+
+// Get rebalances
+async function main() {
+    const calldatas = await getRebalances(provider, FLTS, MIN_PROFITS);
+    console.log(calldatas);
+}
+
+main();
