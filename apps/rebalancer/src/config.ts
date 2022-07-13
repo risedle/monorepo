aa3f0d97e89066f32f41596cbcdf8d09ddf3bb84
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-07-13 13:32:04 +0700

    feat(apps/rebalancer): implement execute(calldatas)

--- apps/rebalancer/src/config.ts
+++ apps/rebalancer/src/config.ts
@@ -6,3 +6,4 @@ dotenv.config();
 export const RPC_URL = process.env.RPC_URL!;
 export const FLTS = process.env.FLTS!.split(",");
 export const MIN_PROFITS = process.env.MIN_PROFITS!.split(",");
+export const WALLET_PK = process.env.WALLET_PK!;
