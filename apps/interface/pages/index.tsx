5aacf3f78ef318b01367e986e2209dc3f3c1555a
pyk <bayualdiyansyah@gmail.com>
2022-07-17 15:59:01 +0700

    feat(apps/interface): setup tailwind (#106)

--- apps/interface/pages/index.tsx
+++ apps/interface/pages/index.tsx
@@ -7,6 +7,7 @@ const Home: NextPage = () => {
     return (
         <div className={styles.container}>
             <Head>
+                <h1 className="text-3xl font-bold underline">Hello world!</h1>
                 <title>Create Next App</title>
                 <meta
                     name="description"
