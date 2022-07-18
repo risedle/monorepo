fa7049db67e1b6ff0928cc5319268b5dc7fcb019
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-07-18 12:49:49 +0700

    feat(apps/interface): implement and test homepage SEO

--- apps/interface/cypress/e2e/home/index.spec.ts
+++
@@ -1,6 +0,0 @@
-describe("My First Test", () => {
-    it("Visits the Kitchen Sink", () => {
-        cy.visit("/");
-        cy.title().should("eq", "Trade Leveraged Tokens | Risedle");
-    });
-});
--- apps/interface/pages/index.tsx
+++ apps/interface/pages/index.tsx
@@ -1,10 +1,15 @@
 import type { NextPage } from "next";
 import { NextSeo } from "next-seo";
+import { getBaseConfig } from "../utils/getBaseConfig";
 
 const Home: NextPage = () => {
+    const baseConfig = getBaseConfig();
+
     return (
         <>
-            <NextSeo title="Trade Leveraged Tokens" />
+            <NextSeo
+                title={`Trade Leveraged Tokens on ${baseConfig.chainName}`}
+            />
             <div>OK</div>
         </>
     );
