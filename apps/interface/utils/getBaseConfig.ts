fe1426465cbd916babd74fe1b4c87f09b866d77c
pyk <bayualdiyansyah@gmail.com>
2022-07-18 12:57:40 +0700

    feat(apps/interface): implement and test homepage SEO (#110)

--- apps/interface/utils/getBaseConfig.ts
+++ apps/interface/utils/getBaseConfig.ts
@@ -9,9 +9,9 @@ interface BaseConfig {
  * Given NEXT_PUBLIC_* from configuration then return the base config
  */
 export function getBaseConfig(): BaseConfig {
-    const chainSlug = process.env.NEXT_PUBLIC_CHAIN_SLUG || "slug";
-    const chainId = parseInt(process.env.NEXT_PUBLIC_CHAIN_ID || "88");
-    const chainName = process.env.NEXT_PUBLIC_CHAIN_NAME || "Slug";
+    const chainSlug = process.env.NEXT_PUBLIC_CHAIN_SLUG || "bsc";
+    const chainId = parseInt(process.env.NEXT_PUBLIC_CHAIN_ID || "56");
+    const chainName = process.env.NEXT_PUBLIC_CHAIN_NAME || "BNB Smart Chain";
     const baseURL = `https://${chainSlug}.risedle.com`;
     return { chainSlug, chainId, chainName, baseURL };
 }
