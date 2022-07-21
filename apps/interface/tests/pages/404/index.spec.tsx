786af457eb34738445d97f7db2065d10edd09df8
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-07-21 19:45:55 +0700

    fix(apps/interface): clear mock on test

--- apps/interface/tests/pages/404/index.spec.tsx
+++ apps/interface/tests/pages/404/index.spec.tsx
@@ -3,6 +3,11 @@ import nextRouter from "next/router";
 import { render, screen } from "@testing-library/react";
 import Custom404 from "../../../pages/404";
 
+afterEach(() => {
+    // restore the spy created with spyOn
+    jest.restoreAllMocks();
+});
+
 describe("<Custom404 />", () => {
     describe("Given a user visit /{{unknown}}", () => {
         it("should render custom 404 page", () => {
