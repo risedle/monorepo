ef4851b17404416ff5bd55b6853d7ac12bb3e02e
Matthew Marcellino <81855912+math-marcellino@users.noreply.github.com>
2022-07-21 23:11:47 +0700

    feat(apps/interface): implement 404 Page (#128)
    * feat(apps/interface): implement 404 page
    
    * chore(apps/interface): refactor 404 page button
    
    * chore(apps/interface): use URL from base config
    
    * chore(apps/interface): change 'landing page' to 'home'
    
    * chore(apps/interface): use '/' on 404 href
    
    * chore(apps/interface): remove unused functions
    
    * feat(apps/interface): test coverage for 404 page
    
    * fix(apps/interface): clear mock on test

---
+++ apps/interface/pages/404.tsx
@@ -0,0 +1,77 @@
+import {
+    Button,
+    Container,
+    Flex,
+    Text,
+    VStack,
+    useColorModeValue,
+} from "@chakra-ui/react";
+import NextLink from "next/link";
+import NavigationBar from "../components/NavigationBar";
+
+export default function Custom404() {
+    const gray10 = useColorModeValue("gray.light.10", "gray.dark.10");
+    const gray12 = useColorModeValue("gray.light.12", "gray.dark.12");
+    return (
+        <Container
+            maxW="7xl"
+            py="3"
+            data-testid="NavigationBar"
+            minH="100vh"
+            display="flex"
+            flexDirection="column"
+        >
+            <NavigationBar />
+            <Flex
+                direction="column"
+                grow={1}
+                gap="64px"
+                alignItems="center"
+                justifyContent="center"
+            >
+                <VStack spacing="32px">
+                    <VStack spacing="16px">
+                        <Text
+                            fontSize="12px"
+                            lineHeight="16px"
+                            align="center"
+                            letterSpacing="widest"
+                            color={gray10}
+                            style={{ textTransform: "uppercase" }}
+                        >
+                            Page Not Found
+                        </Text>
+                        <Text
+                            fontSize="96px"
+                            lineHeight="92px"
+                            fontWeight="bold"
+                            align="center"
+                            letterSpacing="-0.06em"
+                            color={gray12}
+                        >
+                            404
+                        </Text>
+                    </VStack>
+                    <Text
+                        fontSize="16px"
+                        lineHeight="24px"
+                        align="center"
+                        color={gray10}
+                    >
+                        Uh oh, seems the page you're searching is not there
+                    </Text>
+                </VStack>
+                <NextLink href="/" passHref>
+                    <Button
+                        as="a"
+                        variant="gradient"
+                        data-testid="BackToHomeLink"
+                    >
+                        <Text marginRight="8px">&larr;</Text>
+                        Back to Home
+                    </Button>
+                </NextLink>
+            </Flex>
+        </Container>
+    );
+}
