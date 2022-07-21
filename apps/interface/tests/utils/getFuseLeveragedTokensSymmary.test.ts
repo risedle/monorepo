df8bfebcec10704ec7f8d7044596554149de52ba
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-07-21 16:42:42 +0700

    feat(apps/interface): increase test coverage

---
+++ apps/interface/tests/utils/getFuseLeveragedTokensSymmary.test.ts
@@ -0,0 +1,41 @@
+import { getFuseLeveragedTokensSummary } from "../../utils/getFuseLeveragedTokensSummary";
+
+describe("getFuseLeveragedTokensSummary", () => {
+    it("should return summary", () => {
+        const summary = getFuseLeveragedTokensSummary({
+            tokens: [
+                {
+                    dailyData: [
+                        {
+                            close: 10,
+                            totalSupply: 10,
+                            tradeVolumeUSD: 20,
+                        },
+                        {
+                            close: 20,
+                            totalSupply: 20,
+                            tradeVolumeUSD: 30,
+                        },
+                    ],
+                },
+                {
+                    dailyData: [
+                        {
+                            close: 10,
+                            totalSupply: 10,
+                            tradeVolumeUSD: 20,
+                        },
+                        {
+                            close: 20,
+                            totalSupply: 20,
+                            tradeVolumeUSD: 30,
+                        },
+                    ],
+                },
+            ],
+        });
+
+        expect(summary.totalMarketCap).toBe(200);
+        expect(summary.totalVolume).toBe(40);
+    });
+});
