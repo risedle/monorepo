653888d5d15780437206285d5508c32e6ead8094
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-07-22 16:35:18 +0700

    chore(apps/interface): server-side rendered chart

--- apps/interface/components/TokenCard/chart.tsx
+++ apps/interface/components/TokenCard/chart.tsx
@@ -59,73 +59,73 @@ export const TokenCardChart = (props: TokenCardChartProps) => {
     const priceChange = latest.price - oldest.price;
 
     return (
-        <Box h="200px" data-testid="PriceChart" minW="200px !important">
-            <ResponsiveContainer width={300} height={200}>
-                <AreaChart
-                    data={prices}
-                    margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-                >
-                    <Area
-                        type="monotoneX"
-                        dataKey="price"
-                        strokeWidth={2}
-                        stroke={priceChange > 0 ? "#4CC38A" : "#CD2B31"}
-                        fill={
-                            priceChange > 0
-                                ? "url(#upGradient)"
-                                : "url(#downGradient)"
-                        }
-                        data-testid="TokenCardChartArea"
-                    />
-                    <YAxis
-                        hide={true}
-                        type="number"
-                        domain={["dataMin - 5", "dataMax + 5"]}
-                    />
-                    <Tooltip
-                        position={{ y: 0, x: 16 }}
-                        content={<TokenCardChartTooltip />}
-                    />
-                    <defs>
-                        <linearGradient
-                            id="upGradient"
-                            x1="0"
-                            y1="0"
-                            x2="0"
-                            y2="1"
-                        >
-                            <stop
-                                offset="0%"
-                                stopColor="rgba(37, 208, 171)"
-                                stopOpacity={0.4}
-                            />
-                            <stop
-                                offset="100%"
-                                stopColor="rgba(37, 208, 171)"
-                                stopOpacity={0}
-                            />
-                        </linearGradient>
-                        <linearGradient
-                            id="downGradient"
-                            x1="0"
-                            y1="0"
-                            x2="0"
-                            y2="1"
-                        >
-                            <stop
-                                offset="0%"
-                                stopColor="rgb(205, 43, 49)"
-                                stopOpacity={0.4}
-                            />
-                            <stop
-                                offset="100%"
-                                stopColor="rgb(205, 43, 49)"
-                                stopOpacity={0}
-                            />
-                        </linearGradient>
-                    </defs>
-                </AreaChart>
-            </ResponsiveContainer>
+        <Box data-testid="TokenCardChart">
+            <AreaChart
+                data={prices}
+                margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+                width={200}
+                height={100}
+            >
+                <Area
+                    type="monotoneX"
+                    dataKey="price"
+                    strokeWidth={2}
+                    stroke={priceChange > 0 ? "#4CC38A" : "#CD2B31"}
+                    fill={
+                        priceChange > 0
+                            ? "url(#upGradient)"
+                            : "url(#downGradient)"
+                    }
+                    data-testid="TokenCardChartArea"
+                />
+                <YAxis
+                    hide={true}
+                    type="number"
+                    domain={["dataMin - 5", "dataMax + 5"]}
+                />
+                <Tooltip
+                    position={{ y: 0, x: 16 }}
+                    content={<TokenCardChartTooltip />}
+                />
+                <defs>
+                    <linearGradient
+                        id="upGradient"
+                        x1="0"
+                        y1="0"
+                        x2="0"
+                        y2="1"
+                    >
+                        <stop
+                            offset="0%"
+                            stopColor="rgba(37, 208, 171)"
+                            stopOpacity={0.4}
+                        />
+                        <stop
+                            offset="100%"
+                            stopColor="rgba(37, 208, 171)"
+                            stopOpacity={0}
+                        />
+                    </linearGradient>
+                    <linearGradient
+                        id="downGradient"
+                        x1="0"
+                        y1="0"
+                        x2="0"
+                        y2="1"
+                    >
+                        <stop
+                            offset="0%"
+                            stopColor="rgb(205, 43, 49)"
+                            stopOpacity={0.4}
+                        />
+                        <stop
+                            offset="100%"
+                            stopColor="rgb(205, 43, 49)"
+                            stopOpacity={0}
+                        />
+                    </linearGradient>
+                </defs>
+            </AreaChart>
         </Box>
     );
 };
