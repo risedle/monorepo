280903fcfb259e084416c2c81a7cd88b3b525e19
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-07-25 20:25:01 +0700

    fix(apps/interface): fix useFuseLeveragedTokenUserPosition

--- apps/interface/hooks/useFuseLeveragedTokenUserPosition.ts
+++ apps/interface/hooks/useFuseLeveragedTokenUserPosition.ts
@@ -9,13 +9,12 @@ export function useFuseLeveragedTokenUserPosition(
     fltAddress: string,
     userAddress: string | undefined
 ) {
+    // Return early if user addy is undefined
     const { chainId } = getBaseConfig();
     const positionId = `${userAddress}-${fltAddress}`;
     // Set URL to null in order to prevent the request
     // https://swr.vercel.app/docs/conditional-fetching#dependent
-    const url = userAddress
-        ? `https://api.risedle.com/v1/${chainId}/positions/${positionId}`
-        : null;
+    const url = `https://api.risedle.com/v1/${chainId}/positions/${positionId}`;
     const { data, error } = useSWR<FuseLeveragedTokenUserPosition, Error>(
         url,
         fetcher
