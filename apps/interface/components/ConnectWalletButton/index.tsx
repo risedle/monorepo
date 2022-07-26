720065560b5bf4d2190fb6fa89f728951e6b52df
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-07-26 08:30:42 +0700

    fix(apps/interface): blue indicator icon svg name

--- apps/interface/components/ConnectWalletButton/index.tsx
+++ apps/interface/components/ConnectWalletButton/index.tsx
@@ -2,6 +2,7 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { Box, BoxProps, Button, Icon, IconProps } from "@chakra-ui/react";
 
 import { getBaseConfig } from "../../utils/getBaseConfig";
+import BlueIndicator from "../Icons/BlueIndicator";
 
 // TODO(pyk): refactor this
 const RedIndicatorIcon = (props: IconProps) => {
@@ -48,58 +49,6 @@ const RedIndicatorIcon = (props: IconProps) => {
                         mode="normal"
                         in="SourceGraphic"
                         in2="effect1_dropShadow_3427_171249"
-                        result="shape"
-                    />
-                </filter>
-            </defs>
-        </Icon>
-    );
-};
-
-const BlueIndicatorIcon = (props: IconProps) => {
-    return (
-        <Icon
-            data-testid="IndicatorIcon"
-            viewBox="0 0 32 32"
-            color="sky.dark.10"
-            {...props}
-        >
-            <g filter="url(#filter0_d_3427_218392)">
-                <circle cx="16" cy="16.5" r="4" fill="currentColor" />
-            </g>
-            <defs>
-                <filter
-                    id="filter0_d_3427_218392"
-                    x="0"
-                    y="0.5"
-                    width="32"
-                    height="32"
-                    filterUnits="userSpaceOnUse"
-                    color-interpolation-filters="sRGB"
-                >
-                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                    <feColorMatrix
-                        in="SourceAlpha"
-                        type="matrix"
-                        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                        result="hardAlpha"
-                    />
-                    <feOffset />
-                    <feGaussianBlur stdDeviation="6" />
-                    <feComposite in2="hardAlpha" operator="out" />
-                    <feColorMatrix
-                        type="matrix"
-                        values="0 0 0 0 0.541176 0 0 0 0 0.909804 0 0 0 0 1 0 0 0 1 0"
-                    />
-                    <feBlend
-                        mode="normal"
-                        in2="BackgroundImageFix"
-                        result="effect1_dropShadow_3427_218392"
-                    />
-                    <feBlend
-                        mode="normal"
-                        in="SourceGraphic"
-                        in2="effect1_dropShadow_3427_218392"
                         result="shape"
                     />
                 </filter>
@@ -165,7 +114,7 @@ export const ConnectWalletButton = (props: BoxProps) => {
                                     <Button
                                         onClick={openAccountModal}
                                         leftIcon={
-                                            <BlueIndicatorIcon w="6" h="6" />
+                                            <BlueIndicator w="6" h="6" />
                                         }
                                         width="100%"
                                     >
