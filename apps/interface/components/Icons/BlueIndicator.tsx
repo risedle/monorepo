fe03cd02befe0653751e25e629fd0b4872eb9134
Matthew Marcellino <81855912+math-marcellino@users.noreply.github.com>
2022-07-26 11:44:32 +0700

    fix(apps/interface): blue indicator icon svg name (#165)
    * fix(apps/interface): blue indicator icon svg name
    
    * chore(apps/interface): change blue indicator icon component name

---
+++ apps/interface/components/Icons/BlueIndicator.tsx
@@ -0,0 +1,53 @@
+import { Icon, IconProps } from "@chakra-ui/react";
+
+export const BlueIndicatorIcon = (props: IconProps) => (
+    <Icon
+        data-testid="IndicatorIcon"
+        viewBox="0 0 32 32"
+        color="sky.dark.10"
+        {...props}
+    >
+        <g filter="url(#filter0_d_3427_218392)">
+            <circle cx="16" cy="16.5" r="4" fill="currentColor" />
+        </g>
+        <defs>
+            <filter
+                id="filter0_d_3427_218392"
+                x="0"
+                y="0.5"
+                width="32"
+                height="32"
+                filterUnits="userSpaceOnUse"
+                colorInterpolationFilters="sRGB"
+            >
+                <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                <feColorMatrix
+                    in="SourceAlpha"
+                    type="matrix"
+                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                    result="hardAlpha"
+                />
+                <feOffset />
+                <feGaussianBlur stdDeviation="6" />
+                <feComposite in2="hardAlpha" operator="out" />
+                <feColorMatrix
+                    type="matrix"
+                    values="0 0 0 0 0.541176 0 0 0 0 0.909804 0 0 0 0 1 0 0 0 1 0"
+                />
+                <feBlend
+                    mode="normal"
+                    in2="BackgroundImageFix"
+                    result="effect1_dropShadow_3427_218392"
+                />
+                <feBlend
+                    mode="normal"
+                    in="SourceGraphic"
+                    in2="effect1_dropShadow_3427_218392"
+                    result="shape"
+                />
+            </filter>
+        </defs>
+    </Icon>
+);
+
+export default BlueIndicatorIcon;
