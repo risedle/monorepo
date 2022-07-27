11ee12fac5914a885fa7095bc9ef825215dee4a2
Adam Akbar <adam.akbar@ruangguru.com>
2022-07-27 20:46:38 +0700

    feat(apps/interface): hover button style (#180)
    * feat(apps/interface): hover button style
    
    * fix(apps/interface): gradient button transition
    
    * fix(interface/apps): fixing backgroundSize
    
    * chore(interface/apps): prettify code
    
    * fix(interface/apps): fix button light hover
    
    Co-authored-by: ayikfour <ayikfour4@gmail.com>

--- apps/interface/themes/components/Button.ts
+++ apps/interface/themes/components/Button.ts
@@ -12,10 +12,10 @@ const ButtonSolid = ({ colorMode }: { colorMode: string }) => ({
     lineHeight: "4",
     borderRadius: "full",
     _hover: {
-        background: colorMode == "dark" ? "gray.dark.2" : "gray.light.2",
+        background: colorMode == "dark" ? "gray.dark.3" : "gray.light.3",
     },
     _active: {
-        background: colorMode == "dark" ? "gray.dark.2" : "gray.light.2",
+        background: colorMode == "dark" ? "gray.dark.5" : "gray.light.5",
     },
 });
 
@@ -30,10 +30,10 @@ const ButtonIcon = ({ colorMode }: { colorMode: string }) => ({
     borderRadius: "full",
     padding: 0,
     _hover: {
-        background: colorMode == "dark" ? "gray.dark.2" : "gray.light.2",
+        background: colorMode == "dark" ? "gray.dark.3" : "gray.light.3",
     },
     _active: {
-        background: colorMode == "dark" ? "gray.dark.2" : "gray.light.2",
+        background: colorMode == "dark" ? "gray.dark.5" : "gray.light.5",
     },
 });
 
@@ -51,12 +51,24 @@ const ButtonBSC = ({ colorMode }: { colorMode: string }) => ({
     lineHeight: "4",
     borderRadius: "full",
     _hover: {
+        borderColor:
+            colorMode == "dark"
+                ? "bsc.button.border.darker"
+                : "bsc.button.border.lighter",
         background:
-            colorMode == "dark" ? "bsc.button.bg.dark" : "bsc.button.bg.light",
+            colorMode == "dark"
+                ? "bsc.button.bg.darker"
+                : "bsc.button.bg.lighter",
     },
     _active: {
+        borderColor:
+            colorMode == "dark"
+                ? "bsc.button.border.darker"
+                : "bsc.button.border.lighter",
         background:
-            colorMode == "dark" ? "bsc.button.bg.dark" : "bsc.button.bg.light",
+            colorMode == "dark"
+                ? "bsc.button.bg.darkest"
+                : "bsc.button.bg.lightest",
     },
 });
 
@@ -64,26 +76,33 @@ const ButtonGradient = ({ colorMode }: { colorMode: string }) => ({
     backgroundImage:
         colorMode == "dark"
             ? `radial-gradient(
-                                91.36% 358.74% at 12.29% 100%,
-                                #c9bbff 0%,
-                                #b2ecff 30.08%,
-                                #ffc1f9 60.28%,
-                                #fff5c1 100%
-                            );`
+                91.36% 358.74% at 12.29% 100%,
+                #c9bbff 0,
+                #b2ecff 30.08%,
+                #ffc1f9 60.28%,
+                #fff5c1 100%);
+                );`
             : `radial-gradient(
-                            91.36% 358.74% at 12.29% 100%,
-                            #9d85ff 0%,
-                            #7ad4f0 30.08%,
-                            #f554e5 60.28%,
-                            #e7cf55 100%
-                        );`,
+                91.36% 358.74% at 12.29% 100%,
+                #9d85ff 0,#7ad4f0 30.08%,
+                #f554e5 60.28%,
+                #e7cf55 100%
+                );`,
     color: colorMode == "dark" ? "gray.light.12" : "gray.light.1",
     px: "6",
     py: "3",
     fontWeight: "semibold",
     fontSize: "sm",
+    backgroundSize: "100% 100%",
     lineHeight: "4",
     borderRadius: "full",
+    transitionProperty: "all",
+    transitionDuration: ".3s",
+    transitionTimingFunction: "cubic-bezier(.4,0,.2,1)",
+    _hover: {
+        backgroundSize: "300% 300%",
+        boxShadow: "0px 4px 24px rgba(255, 255, 255, 0.12)",
+    },
 });
 
 const Button = {
