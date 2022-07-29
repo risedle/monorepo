527f46a200f8a744f14afc0968ab552285a95061
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-07-30 00:00:34 +0700

    chore(apps/interface): remove comment

--- apps/interface/utils/parseSlippageTolerance.ts
+++ apps/interface/utils/parseSlippageTolerance.ts
@@ -1,7 +1,6 @@
 export function parseSlippageTolerance(
     setSlippage: (slippage: string) => void
 ): (slippage: string) => void {
-    // test
     return (value: string) => {
         const slippage = value.replace(",", ".");
         setSlippage(slippage);
