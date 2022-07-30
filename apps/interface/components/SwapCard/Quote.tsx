262a091e7d5c39db924cd993de3081f848d548b3
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-07-30 11:26:27 +0700

    feat(apps/interface): implement SwapCard Quote (stateless)

---
+++ apps/interface/components/SwapCard/Quote.tsx
@@ -0,0 +1,82 @@
+import {
+    VStack,
+    Flex,
+    Text,
+    Divider,
+    Box,
+    useColorModeValue,
+    Spacer,
+    Skeleton,
+} from "@chakra-ui/react";
+
+// Utils
+import formatTokenBalance from "@/utils/formatTokenBalance";
+
+interface SwapCardQuoteProps {
+    label: string;
+    quoteAmount: string;
+    quoteSymbol: string;
+    isLoaded: boolean;
+}
+
+export const SwapCardQuote = (props: SwapCardQuoteProps) => {
+    // Data
+    const { label, quoteAmount, quoteSymbol, isLoaded } = props;
+
+    // Styles
+    const gray2 = useColorModeValue("gray.light.2", "gray.dark.2");
+    const gray3 = useColorModeValue("gray.light.3", "gray.dark.3");
+    const gray4 = useColorModeValue("gray.light.4", "gray.dark.4");
+    const gray5 = useColorModeValue("gray.light.5", "gray.dark.5");
+    const gray10 = useColorModeValue("gray.light.10", "gray.dark.10");
+
+    return (
+        <VStack
+            data-testid="SwapCardQuote"
+            margin="0 !important"
+            width="100%"
+            paddingX="4"
+            background={gray2}
+        >
+            <Flex width="100%" alignItems="center">
+                <Text fontSize="xs" lineHeight="4" color={gray10}>
+                    {label}
+                </Text>
+                <Box flex="1" paddingLeft="2">
+                    <Divider borderStyle="dashed" borderColor={gray5} />
+                </Box>
+            </Flex>
+            <Flex width="100%" alignItems="center">
+                <Skeleton
+                    isLoaded={isLoaded}
+                    startColor={gray3}
+                    endColor={gray4}
+                    borderRadius="lg"
+                    minW="60px"
+                >
+                    <Text
+                        fontFamily="mono"
+                        fontWeight="semibold"
+                        fontSize="xl"
+                        letterSpacing="tight"
+                        lineHeight="6"
+                    >
+                        {formatTokenBalance(parseFloat(quoteAmount) || 0)}
+                    </Text>
+                </Skeleton>
+                <Spacer />
+                <Text
+                    fontSize="md"
+                    lineHeight="4"
+                    letterSpacing="tight"
+                    fontFamily="mono"
+                    color={gray10}
+                >
+                    {quoteSymbol}
+                </Text>
+            </Flex>
+        </VStack>
+    );
+};
+
+export default SwapCardQuote;
