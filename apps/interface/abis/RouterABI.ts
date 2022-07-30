a36f9ad0b7ae96a7f8cc24d6da01639dfebe1e35
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-07-30 15:30:08 +0700

    feat(apps/interface): implement SwapCard SellQuoteContainer

--- apps/interface/abis/RouterABI.ts
+++ apps/interface/abis/RouterABI.ts
@@ -3,6 +3,7 @@ import { ethers } from "ethers";
 export const RouterABI = new ethers.utils.Interface([
     // Read
     "function getAmountIn(address _flt, address _tokenIn, uint256 _amountOut) external view returns (uint256 _amountIn)",
+    "function getAmountOut(address _flt, address _tokenOut, uint256 _amountIn) external view returns (uint256 _amountOut)",
 ]);
 
 export default RouterABI;
