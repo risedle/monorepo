4b48cd6d2fc668d9095a28656ce645c18788fb35
Matthew Marcellino <81855912+math-marcellino@users.noreply.github.com>
2022-08-10 12:57:31 +0700

    chore(apps/interface): remove bscscan link (#243)
    * chore(apps/interface): remove bscscan link
    
    * chore(apps/interface): remove unused code
    
    Co-authored-by: pyk <2213646+pyk@users.noreply.github.com>

--- apps/interface/components/FuseLeveragedTokenInfoCard/index.tsx
+++ apps/interface/components/FuseLeveragedTokenInfoCard/index.tsx
@@ -2,24 +2,12 @@ import {
     BoxProps,
     VStack,
     Text,
-    HStack,
     useColorModeValue,
-    Link,
-    Center,
-    Tooltip,
     Divider,
 } from "@chakra-ui/react";
-import { utils } from "ethers";
 
 // Utils
 import type { FuseLeveragedToken } from "@/utils/types";
-import getBaseConfig from "@/utils/getBaseConfig";
-import getTokenExplorerURL from "@/utils/getTokenExplorerURL";
-import formatTokenAddress from "@/utils/formatTokenAddress";
-
-// Icons
-import GlobeIcon from "../Icons/Globe";
-import ArrowTopRightIcon from "../Icons/ArrowTopRight";
 
 // Sub-components
 import FuseLeveragedTokenInfoCardStatsContainer from "./StatsContainer";
@@ -31,9 +19,6 @@ interface FuseLeveragedTokenInfoCardProps extends BoxProps {
 export const FuseLeveragedTokenInfoCard = (
     props: FuseLeveragedTokenInfoCardProps
 ) => {
-    // Global config
-    const { explorerName } = getBaseConfig();
-
     // Data
     const { flt, ...boxProps } = props;
     const { name, symbol, address, collateral, debt } = flt;
@@ -91,51 +76,6 @@ export const FuseLeveragedTokenInfoCard = (
                 </Text>
             </VStack>
 
-            {/* Contract Link */}
-            <HStack margin="0 !important" paddingX="4" gap={2}>
-                <GlobeIcon w="4" h="4" />
-                <Text
-                    color={gray10}
-                    fontSize="sm"
-                    lineHeight="4"
-                    margin="0 !important"
-                >
-                    {explorerName}
-                </Text>
-                <Tooltip
-                    label="Don't trust, verify"
-                    placement="top"
-                    background={gray12}
-                    color={gray2}
-                    boxShadow="none"
-                    data-testid="InfoTooltip"
-                    fontSize="sm"
-                    lineHeight="4"
-                    borderRadius="lg"
-                    padding="2"
-                >
-                    <Link
-                        href={getTokenExplorerURL(address)}
-                        target="_blank"
-                        margin="0 !important"
-                        _hover={{ textDecoration: "none" }}
-                    >
-                        <Center>
-                            <Text
-                                fontFamily="mono"
-                                fontSize="sm"
-                                lineHeight="4"
-                                letterSpacing="tight"
-                                color={gray12}
-                            >
-                                {formatTokenAddress(utils.getAddress(address))}
-                            </Text>
-                            <ArrowTopRightIcon w="4" h="4" color={gray10} />
-                        </Center>
-                    </Link>
-                </Tooltip>
-            </HStack>
-
             {/* Divider */}
             <Divider
                 borderStyle="dashed"
@@ -150,6 +90,7 @@ export const FuseLeveragedTokenInfoCard = (
                 symbol={symbol}
                 collateralSymbol={collateral.symbol}
                 debtSymbol={debt.symbol}
+                address={address}
             />
         </VStack>
     );
