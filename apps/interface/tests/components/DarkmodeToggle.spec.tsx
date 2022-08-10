fa477aa19a686f932d537ebe43518512a862a298
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-08-10 14:31:04 +0700

    chore(apps/interface): fix test for DarkmodeToggle component

--- apps/interface/tests/components/DarkmodeToggle.spec.tsx
+++ apps/interface/tests/components/DarkmodeToggle.spec.tsx
@@ -1,53 +1,25 @@
-import { screen, render } from "@testing-library/react";
-import chakra from "../utils/chakra";
+import { screen, fireEvent, waitFor } from "@testing-library/react";
+import renderApp from "../utils/renderApp";
 
 import { DarkmodeToggle } from "@/components/DarkmodeToggle";
 
-afterEach(() => {
-    // restore the spy created with spyOn
-    jest.restoreAllMocks();
-});
-
-describe("Given useColorMode == 'dark'", () => {
-    it("<DarkmodeIcon /> should be rendered", () => {
-        const useColorMode = jest.spyOn(chakra, "useColorMode");
-        useColorMode.mockReturnValue({ colorMode: "dark" });
-
-        render(<DarkmodeToggle />);
-        setTimeout(() => {
-            const darkmodeIcon = screen.queryByTestId("DarkmodeIcon");
-            expect(darkmodeIcon).toBeInTheDocument();
-        }, 500);
+describe("Given <DarkmodeToggle /> is rendered", () => {
+    beforeEach(() => {
+        renderApp(<DarkmodeToggle />);
     });
 
-    it("<LightmodeIcon /> should not be rendered", () => {
-        const useColorMode = jest.spyOn(chakra, "useColorMode");
-        useColorMode.mockReturnValue({ colorMode: "dark" });
-
-        render(<DarkmodeToggle />);
-        setTimeout(() => {
-            const lightmodeIcon = screen.queryByTestId("LightmodeIcon");
-            expect(lightmodeIcon).not.toBeInTheDocument();
-        }, 500);
-    });
-});
-
-describe("Given useColorMode == 'light'", () => {
-    it("<LightmodeIcon /> should be rendered", () => {
-        const useColorMode = jest.spyOn(chakra, "useColorMode");
-        useColorMode.mockReturnValue({ colorMode: "light" });
-
-        render(<DarkmodeToggle />);
+    it("should render lightmode icon by default", () => {
         const lightmodeIcon = screen.queryByTestId("LightmodeIcon");
         expect(lightmodeIcon).toBeInTheDocument();
     });
 
-    it("<DarkmodeIcon /> should not be rendered", () => {
-        const useColorMode = jest.spyOn(chakra, "useColorMode");
-        useColorMode.mockReturnValue({ colorMode: "light" });
+    it("should render darkmode icon when clicked", async () => {
+        const toggle = screen.queryByTestId("DarkmodeToggle");
+        fireEvent.click(toggle);
 
-        render(<DarkmodeToggle />);
-        const darkmodeIcon = screen.queryByTestId("DarkmodeIcon");
-        expect(darkmodeIcon).not.toBeInTheDocument();
+        await waitFor(() => {
+            const darkmodeIcon = screen.queryByTestId("DarkmodeIcon");
+            expect(darkmodeIcon).toBeInTheDocument();
+        });
     });
 });
