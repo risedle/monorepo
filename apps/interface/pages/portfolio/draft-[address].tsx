620a05b387daf14b1b8009ce2c7ffe056290b296
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-08-12 13:29:26 +0700

    feat(apps/interface): add new portfolio page draft

---
+++ apps/interface/pages/portfolio/draft-[address].tsx
@@ -0,0 +1,51 @@
+import { NextPage } from "next";
+import { NextSeo } from "next-seo";
+import { Container } from "@chakra-ui/react";
+
+import getBaseConfig from "@/utils/getBaseConfig";
+
+import NavigationBar from "@/components/NavigationBar";
+import { NavigationBarBottom } from "@/components/NavigationBarBottom";
+import { BackgroundGradient } from "@/components/BackgroundGradient";
+
+interface PortfolioAddressProps {}
+
+const PortfolioAddress: NextPage<PortfolioAddressProps> = () => {
+    const { chainName, baseURL, chainSlug } = getBaseConfig();
+    return (
+        <>
+            <NextSeo
+                title={`Trade Leveraged Tokens on ${chainName}`}
+                openGraph={{
+                    type: "website",
+                    url: baseURL,
+                    title: `Trade Leveraged Tokens on ${chainName}`,
+                    description:
+                        "Trade, earn and build on the decentralized crypto leveraged token protocol",
+                    site_name: "Risedle",
+                    images: [
+                        {
+                            url: `https://${chainSlug}.risedle.com/opengraph/portfolioAddress-${chainSlug}.png`,
+                        },
+                    ],
+                }}
+            />
+            <NavigationBar />
+            <Container
+                maxW="5xl"
+                py="3"
+                data-testid="PortfolioAddressContent"
+                marginTop={{ base: "20px", laptop: "16" }}
+            >
+                {/* Content Here */}
+            </Container>
+            <NavigationBarBottom />
+            <BackgroundGradient
+                page="trade"
+                display={{ base: "none", laptop: "block" }}
+            />
+        </>
+    );
+};
+
+export default PortfolioAddress;
