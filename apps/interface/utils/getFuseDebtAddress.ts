fc2e595407be1d9a9080cef2cbc2a9746d739348
pyk <bayualdiyansyah@gmail.com>
2022-08-12 15:44:10 +0700

    fix(apps/interface): add CAKERISE and CAKEDROP fDebt (#258)

--- apps/interface/utils/getFuseDebtAddress.ts
+++ apps/interface/utils/getFuseDebtAddress.ts
@@ -1,6 +1,8 @@
 const fdebts: Record<string, string> = {
     BNBRISE: "0x1f6b34d12301d6bf0b52db7938fc90ab4f12fe95",
     BNBDROP: "0xFEc2B82337dC69C61195bCF43606f46E9cDD2930",
+    CAKERISE: "0x1f6b34d12301d6bf0b52db7938fc90ab4f12fe95",
+    CAKEDROP: "0xbd163D07015ae3c0701304E20FEBAB421A2020aA",
 };
 
 export const getFuseDebtAddress = (symbol: string): string => {
