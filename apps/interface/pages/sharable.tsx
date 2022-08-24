5059021ab3b8bad1d5f8a418247e092929b40dfb
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-08-24 18:22:40 +0700

    chore(apps/interface): disable eslint for empty interface

--- apps/interface/pages/sharable.tsx
+++ apps/interface/pages/sharable.tsx
@@ -14,6 +14,7 @@ import {
     Button,
 } from "@chakra-ui/react";
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface SharableProps {}
 
 const Sharable: NextPage<SharableProps> = () => {
