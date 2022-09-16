84887f72c25aaab205d47163ce92d785ceb13c92
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-09-17 04:13:42 +0700

    feat(monorepo): upload coverage to vercel

---
+++ coverage/build.js
@@ -0,0 +1,40 @@
+// Collect coverage and list them in this folder
+const glob = require("glob");
+const fse = require("fs-extra");
+const Mustache = require("mustache");
+
+const index = `
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <ul>
+        {{#sources}}
+            <li>
+                <a href="/{{{.}}}">
+                    {{{.}}}
+                </a>
+            </li>
+        {{/sources}}
+    </ul>
+</body>
+</html>
+`;
+
+// options is optional
+glob(
+    "**/coverage/lcov-report",
+    { ignore: "node_modules/**" },
+    function (er, files) {
+        files.forEach((srcDir) => {
+            const destDir = "coverage/" + srcDir;
+            fse.copySync(srcDir, destDir, { overwrite: true | false });
+        });
+        const output = Mustache.render(index, { sources: files });
+        // With a callback:
+        fse.outputFile("./coverage/index.html", output);
+    }
+);
