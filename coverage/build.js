b3ed996760f3cd3a55c838206cb240905b506503
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-09-17 04:34:46 +0700

    chore(coverage): add web title

--- coverage/build.js
+++ coverage/build.js
@@ -7,6 +7,7 @@ const index = `
 <!doctype html>
 <html>
 <head>
+    <title>Risedle Monorepo Coverage</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
