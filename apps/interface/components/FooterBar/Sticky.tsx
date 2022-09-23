b2066ec2b2ee7205625ed45f337ca4d139e0ac4d
Matthew Marcellino <81855912+math-marcellino@users.noreply.github.com>
2022-09-23 14:42:51 +0700

    chore(apps/interface): refactor StickyFooterBar (#361)
    ## Scope
    List of affected projects:
    
    - apps/interface
    
    ## Description
    We need to refactor the `StickyFooterBar` component.
    
    The rendered element should have the following markup:
    
    ```html
    <footer>
    		<nav>
    				<ul>
    						<li><a>link</a></li>
    						<li><a>link</a></li>
    						<li><a>link</a></li>
    						<li><a>link</a></li>
    				</ul>
    		</nav>
    		<p>copyright</p>
    </footer>
    ```
    
    ## Action items
    Action items for this pull request:
    
    - [x] Refactor `StickyFooterBar`
    - [x] Use default import for all utils and sub-componens (if any)
    
    ## Checklist
    You should check this before requesting for review:
    
    - [x] Branch name use the the following format RIS-{NUMBER}
    - [x] Pull request title is "chore(apps/interface): refactor
    StickyFooterBar"
    - [x] Make sure new files are 100% covered in [Risedle Code
    Coverage](https://coverage.risedle.com)
    
    Co-authored-by: pyk <gm@pyk.sh>

--- apps/interface/components/FooterBar/Sticky.tsx
+++ apps/interface/components/FooterBar/Sticky.tsx
@@ -1,10 +1,12 @@
 import {
     VStack,
     Link,
-    HStack,
     Text,
     useColorModeValue,
     Center,
+    Box,
+    List,
+    ListItem,
 } from "@chakra-ui/react";
 import NextLink from "next/link";
 
@@ -18,6 +20,7 @@ const StickyFooterBar = () => {
 
     return (
         <VStack
+            as="footer"
             data-testid="StickyFooterBar"
             marginTop="4"
             gap="4"
@@ -25,87 +28,97 @@ const StickyFooterBar = () => {
             alignItems="center"
             py="6"
         >
-            <HStack gap="6">
-                <NextLink href="https://twitter.com/risedle" passHref>
-                    <Link
-                        fontSize="sm"
-                        lineHeight="4"
-                        color={gray10}
-                        _hover={{ color: gray12 }}
-                        target="_blank"
-                        data-testid="FooterBarLinkTwitter"
-                    >
-                        <Center>
-                            Twitter
-                            <ArrowTopRightIcon
-                                w="14px"
-                                h="14px"
+            <Box as="nav">
+                <List display="flex" flexDirection="row" gap="6" as="ul">
+                    <ListItem>
+                        <NextLink href="https://twitter.com/risedle" passHref>
+                            <Link
+                                fontSize="sm"
+                                lineHeight="4"
                                 color={gray10}
-                            />
-                        </Center>
-                    </Link>
-                </NextLink>
-                <NextLink
-                    href="https://discord.com/invite/YCSCd97SXj"
-                    passHref
-                >
-                    <Link
-                        fontSize="sm"
-                        lineHeight="4"
-                        color={gray10}
-                        _hover={{ color: gray12 }}
-                        target="_blank"
-                        data-testid="FooterBarLinkDiscord"
-                    >
-                        <Center>
-                            Discord
-                            <ArrowTopRightIcon
-                                w="14px"
-                                h="14px"
+                                _hover={{ color: gray12 }}
+                                target="_blank"
+                                data-testid="FooterBarLinkTwitter"
+                            >
+                                <Center>
+                                    Twitter
+                                    <ArrowTopRightIcon
+                                        w="14px"
+                                        h="14px"
+                                        color={gray10}
+                                    />
+                                </Center>
+                            </Link>
+                        </NextLink>
+                    </ListItem>
+                    <ListItem>
+                        <NextLink
+                            href="https://discord.com/invite/YCSCd97SXj"
+                            passHref
+                        >
+                            <Link
+                                fontSize="sm"
+                                lineHeight="4"
                                 color={gray10}
-                            />
-                        </Center>
-                    </Link>
-                </NextLink>
-                <NextLink href="https://github.com/risedle" passHref>
-                    <Link
-                        fontSize="sm"
-                        lineHeight="4"
-                        color={gray10}
-                        _hover={{ color: gray12 }}
-                        target="_blank"
-                        data-testid="FooterBarLinkGithub"
-                    >
-                        <Center>
-                            Github
-                            <ArrowTopRightIcon
-                                w="14px"
-                                h="14px"
+                                _hover={{ color: gray12 }}
+                                target="_blank"
+                                data-testid="FooterBarLinkDiscord"
+                            >
+                                <Center>
+                                    Discord
+                                    <ArrowTopRightIcon
+                                        w="14px"
+                                        h="14px"
+                                        color={gray10}
+                                    />
+                                </Center>
+                            </Link>
+                        </NextLink>
+                    </ListItem>
+                    <ListItem>
+                        <NextLink href="https://github.com/risedle" passHref>
+                            <Link
+                                fontSize="sm"
+                                lineHeight="4"
                                 color={gray10}
-                            />
-                        </Center>
-                    </Link>
-                </NextLink>
-                <NextLink href="https://docs.risedle.com" passHref>
-                    <Link
-                        fontSize="sm"
-                        lineHeight="4"
-                        color={gray10}
-                        _hover={{ color: gray12 }}
-                        target="_blank"
-                        data-testid="FooterBarLinkDocs"
-                    >
-                        <Center>
-                            Docs
-                            <ArrowTopRightIcon
-                                w="14px"
-                                h="14px"
+                                _hover={{ color: gray12 }}
+                                target="_blank"
+                                data-testid="FooterBarLinkGithub"
+                            >
+                                <Center>
+                                    Github
+                                    <ArrowTopRightIcon
+                                        w="14px"
+                                        h="14px"
+                                        color={gray10}
+                                    />
+                                </Center>
+                            </Link>
+                        </NextLink>
+                    </ListItem>
+                    <ListItem>
+                        <NextLink href="https://docs.risedle.com" passHref>
+                            <Link
+                                fontSize="sm"
+                                lineHeight="4"
                                 color={gray10}
-                            />
-                        </Center>
-                    </Link>
-                </NextLink>
-            </HStack>
+                                _hover={{ color: gray12 }}
+                                target="_blank"
+                                data-testid="FooterBarLinkDocs"
+                            >
+                                <Center>
+                                    Docs
+                                    <ArrowTopRightIcon
+                                        w="14px"
+                                        h="14px"
+                                        color={gray10}
+                                    />
+                                </Center>
+                            </Link>
+                        </NextLink>
+                    </ListItem>
+                </List>
+            </Box>
             <Text fontSize="sm" lineHeight="4" color={gray10}>
                 &copy; 2022 Risedle Labs
             </Text>
