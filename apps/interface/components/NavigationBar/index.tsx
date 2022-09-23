ae543f45d4a188615e0137296cb027d5e294500e
Matthew Marcellino <81855912+math-marcellino@users.noreply.github.com>
2022-09-23 14:58:20 +0700

    chore(apps/interface): refactor ConnectWallet (#362)
    ## Scope
    List of affected projects:
    
    - apps/interface
    
    ## Description
    We need to refactor the `ConnectWallet` button component
    
    ## Action items
    Action items for this pull request:
    
    - [x] Use default import for all utils and components
    - [x] Remove individual export
    - [x] Add default export
    
    ## Checklist
    You should check this before requesting for review:
    
    - [x] Branch name use the the following format RIS-{NUMBER}
    - [x] Pull request title is "chore(apps/interface): refactor
    ConnectWallet"
    - [x] Make sure new files are 100% covered in [Risedle Code
    Coverage](https://coverage.risedle.com)
    
    Co-authored-by: pyk <gm@pyk.sh>

--- apps/interface/components/NavigationBar/index.tsx
+++ apps/interface/components/NavigationBar/index.tsx
@@ -3,7 +3,7 @@ import { Container, Flex, Center, Spacer, HStack } from "@chakra-ui/react";
 import { NavigationBarLogo } from "./logo";
 import { NavigationBarLinks } from "./links";
 import ChainSwitcher from "../ChainSwitcher";
-import { ConnectWalletButton } from "../ConnectWalletButton";
+import ConnectWalletButton from "../ConnectWalletButton";
 import DarkmodeToggle from "../DarkmodeToggle";
 
 export const NavigationBar = () => {
