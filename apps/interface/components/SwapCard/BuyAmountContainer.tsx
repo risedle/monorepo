18512672b14e31762cb37ed5fe258cf62e1b6f9e
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-10-06 22:11:43 +0700

    chore(apps/interface): refactor SwapCardBuyAmountContainer

--- apps/interface/components/SwapCard/BuyAmountContainer.tsx
+++ apps/interface/components/SwapCard/BuyAmountContainer.tsx
@@ -11,7 +11,7 @@ interface SwapCardBuyAmountContainerProps {
     symbol: string;
 }
 
-export const SwapCardBuyAmountContainer = (
+const SwapCardBuyAmountContainer = (
     props: SwapCardBuyAmountContainerProps
 ) => {
     // Data
