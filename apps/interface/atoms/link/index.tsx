f716b06474e8c9093f78a2664446c6d5e12eaf2b
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-10-16 00:01:04 +0700

    feat(apps/interface): implment Link

--- apps/interface/atoms/link/index.tsx
+++ apps/interface/atoms/link/index.tsx
@@ -1,7 +1,15 @@
+/**
+ * Link
+ *
+ * Link renders Anchor HTML tag.
+ */
 import React from "react";
 
+import type { UtilityProps } from "~/atoms/box/utilities";
 import Box from "~/atoms/box";
 
-export default function Link() {
-    return <Box as="a" />;
+export default function Link(
+    props: React.ComponentPropsWithoutRef<"a"> & UtilityProps
+) {
+    return <Box as="a" {...props} />;
 }
